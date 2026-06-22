3e198d082adc67f07990f6b0c83f1c4597e1f8a4
ondrejnyklicek <ondrejnyklicek@icloud.com>
2026-06-22 16:50:26 +0200

    Fix divider Small improvements

---
+++ packages/core/resources/views/actions/partials/divider.blade.php
@@ -0,0 +1,2 @@
+{{-- Visual separator for ActionGroup dropdowns. Shared by Action::render() and the table action-group view. --}}
+<div class="border-t border-gray-100 dark:border-gray-700 my-1" role="separator"></div>
